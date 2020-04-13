bfbb4a7c196cbfe1a816a83b4efbbd2e590b4442
鬼澤英里 <kizawa-eri@g.ecc.u-tokyo.ac.jp>
2020-04-13 22:53:56 +0900

    0413

--- アルゴリズムとデータ構造/alds1_2_a.py
+++ アルゴリズムとデータ構造/alds1_2_a.py
@@ -2,6 +2,8 @@ N=int(input())
 lst=list(map(int,input().split()))
 
 swap=0
+
+flag = 1
 while flag:
     flag = 0
     for i in reversed(list(range(1,len(lst)))):
@@ -9,3 +11,6 @@ while flag:
             lst[i-1],lst[i] = lst[i],lst[i-1]
             flag = 1
             swap+=1
+
+print(' '.join(map(str,lst)))
+print(swap)
