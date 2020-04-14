10dd39debe3de7272d6bfbe6b5a1a064c480ee0a
鬼澤英里 <kizawa-eri@g.ecc.u-tokyo.ac.jp>
2020-04-14 18:11:35 +0900

    finished up to section 4

--- spiral/alds1_3_d.py
+++ spiral/alds1_3_d.py
@@ -6,15 +6,24 @@ s1 = deque()
 s2 = deque()
 
 n = len(line)
-area = 0
+total_area = 0
 
 for i in range(n):
-    if line[i] == "\":
+    if line[i] == "\\":
         s1.append(i)
     elif line[i] == "/":
         if s1: # if s1 is not empty
-            dis = i - s1.pop()
-            area += dis
-        
+            j = s1.pop()
+            area = i - j
+            total_area += area
+            if s2 and j < s2[-1][0]: # integrate the area of ponds
+                while s2 and j < s2[-1][0]:
+                    pre_pond = s2.pop()[1]
+                    area += pre_pond
+            s2.append([j,area])
 
-
+print(total_area) 
+if total_area!=0:
+    print("{} ".format(len(s2)) + " ".join([str(x[1]) for x in s2]))
+else:
+    print(0)
